c5d687fb05a76d60874da0d11c6e8b6fd34971b6
Isaac Tsai <isaactsai6@gmail.com>
2021-06-18 16:01:46 -0500

    Database improvements
    Attempting to store verse count length for every chapter in each book

--- bibleRegex.js
+++ bibleRegex.js
@@ -1,99 +1,3 @@
-var books = [
-    'Genesis',         'Exodus',          'Leviticus',     'Numbers',
-    'Deuteronomy',     'Joshua',          'Judges',        'Ruth',
-    '1Samuel',        '2Samuel',        '1Kings',       '2Kings',
-    '1Chronicles',    '2Chronicles',    'Ezra',          'Nehemiah',
-    'Esther',          'Job',             'Psalms',         'Proverbs',
-    'Ecclesiastes',    'SongofSolomon', 'Isaiah',        'Jeremiah',
-    'Lamentations',    'Ezekiel',         'Daniel',        'Hosea',
-    'Joel',            'Amos',            'Obadiah',       'Jonah',
-    'Micah',           'Nahum',           'Habakkuk',      'Zephaniah',
-    'Haggai',          'Zechariah',       'Malachi',       'Matthew',
-    'Mark',            'Luke',            'John',          'Acts',
-    'Romans',          '1Corinthians',   '2Corinthians', 'Galatians',
-    'Ephesians',       'Philippians',     'Colossians',    '1Thessalonians', 
-    '2Thessalonians', '1Timothy',       '2Timothy',     'Titus',
-    'Philemon',        'Hebrews',         'James',         '1Peter',
-    '2Peter',         '1John',          '2John',        '3John',
-    'Jude',            'Revelation'
-];
-
-var booksAndInfo = {
-    'Genesis': {
-        chapterCount: 50,
-        chapters: {
-            1: {
-                verses: 31 
-            }
-        }
-    },
-    'Exodus': [40],
-    'Leviticus': [27],
-    'Numbers': [36],
-    'Deuteronomy': [34],
-    'Joshua': [24],
-    'Judges': [21],
-    'Ruth': [4],
-    '1Samuel': [31],
-    '2Samuel': [24],
-    '1Kings': [22],
-    '2Kings': [25],
-    '1Chronicles': [29],
-    '2Chronicles': [36],
-    'Ezra': [10],
-    'Nehemiah': [13],
-    'Esther': [10],
-    'Job': [42],
-    'Psalms': [150],
-    'Proverbs': [31],
-    'Ecclesiastes': [12],
-    'SongofSolomon': [8],
-    'Isaiah': [66],
-    'Jeremiah': [52],
-    'Lamentations': [5],
-    'Ezekiel': [48],
-    'Daniel': [12],
-    'Hosea': [14],
-    'Joel': [3],
-    'Amos': [9],
-    'Obadiah': [1],
-    'Jonah': [4],
-    'Micah': [7],
-    'Nahum': [3],
-    'Habakkuk': [3],
-    'Zephaniah': [3],
-    'Haggai': [2],
-    'Zechariah': [14],
-    'Malachi': [4],
-    'Matthew': [28],
-    'Mark': [16],
-    'Luke': [24],
-    'John': [21],
-    'Acts': [28],
-    'James': [5],
-    '1Peter': [5],
-    '2Peter': [3],
-    '1John': [5],
-    '2John': [1],
-    '3John': [1],
-    'Jude': [1],
-    'Romans': [16],
-    '1Corinthians': [16],
-    '2Corinthians': [13],
-    'Galatians': [6],
-    'Ephesians': [6],
-    'Philippians': [4],
-    'Colossians': [4],
-    '1Thessalonians': [5],
-    '2Thessalonians': [3],
-    '1Timothy': [6],
-    '2Timothy': [4],
-    'Titus': [3],
-    'Philemon': [1],
-    'Hebrews': [13],
-    'Revelation': [22]
-}
-
 var masterArray = 
 {
     "Genesis":{
@@ -361,3 +265,72 @@ var masterArray =
        "chapters":2 [2]
     }
 };
+
+const verseCountForChapter = async (book, chapter, ref) => {
+    var ESVHeaders = new Headers();
+    ESVHeaders.append("Authorization", "Token 21e8c9c7473c40e539c611b6b89b431e3e84514a");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: ESVHeaders,
+        redirect: 'follow'
+    };
+
+
+    let response = await fetch(`https://api.esv.org/v3/passage/text/?q=${book}+${chapter}`, requestOptions).then(response => response.json()).then(result => {
+        let verseCount = result.passages[0].match(/(\[\d{1,4}\])/gm).length;
+        console.log(result.passages[0].match(/(\[\d{1,4}\])/gm), book, chapter, 'verseCount:' + verseCount)
+        ref.push(verseCount)
+    })  
+
+    if (masterArray[book].chapterCount < masterArray[book].chapters.length) {
+        console.groupEnd()
+        console.log(book + ' is finished', masterArray[book])
+        goNext(book)
+    }
+}
+
+
+const push = (key) => {
+    let book = masterArray[key]
+    console.log('current working book: ' + key)
+    book.chapters = [0]
+
+    document.body.innerHTML = "";
+    document.write('current working book: ' + key)
+
+    console.groupCollapsed()
+
+    for (let i = 1; i < book.chapterCount+1; i++) {
+        const action = () => {
+            verseCountForChapter(key, i, book.chapters)
+        }
+        setTimeout(action, i*2000)
+    }
+}
+
+//push('1Samuel')
+
+var books = [
+    '1Samuel',        '2Samuel',        '1Kings',       '2Kings',
+    '1Chronicles',    '2Chronicles',    'Ezra',          'Nehemiah',
+    'Esther',          'Job',             'Psalms',         'Proverbs',
+    'Ecclesiastes',    'SongofSolomon', 'Isaiah',        'Jeremiah',
+    'Lamentations',    'Ezekiel',         'Daniel',        'Hosea',
+    'Joel',            'Amos',            'Obadiah',       'Jonah',
+    'Micah',           'Nahum',           'Habakkuk',      'Zephaniah',
+    'Haggai',          'Zechariah',       'Malachi',       'Matthew',
+    'Mark',            'Luke',            'John',          'Acts',
+    'Romans',          '1Corinthians',   '2Corinthians', 'Galatians',
+    'Ephesians',       'Philippians',     'Colossians',    '1Thessalonians', 
+    '2Thessalonians', '1Timothy',       '2Timothy',     'Titus',
+    'Philemon',        'Hebrews',         'James',         '1Peter',
+    '2Peter',         '1John',          '2John',        '3John',
+    'Jude',            'Revelation'
+];
+
+function goNext (prevBook) {
+    var nextBookIndex = (books.indexOf(prevBook) + 1);
+    push(books[nextBookIndex])
+}
+
