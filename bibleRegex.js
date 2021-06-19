b3ce8a2b2eb8a2593bdfa49b2afd49dc1518717d
Isaac Tsai <isaactsai6@gmail.com>
2021-06-19 11:56:41 -0500

    Database additions

--- bibleRegex.js
+++ bibleRegex.js
@@ -34,67 +34,67 @@ var masterArray =
     },
     "1Samuel":{
        "chapterCount":31,
-       "chapters":2
+       "chapters":[0,28,36,21,22,12,21,17,22,27,27,15,25,23,52,35,23,58,30,24,43,15,23,29,22,44,25,12,25,11,31,13]
     },
     "2Samuel":{
        "chapterCount":24,
-       "chapters":2
+       "chapters":[0,27,32,39,12,25,24,29,18,13,19,27,31,39,33,37,23,29,33,43,26,22,51,39,25]
     },
     "1Kings":{
        "chapterCount":22,
-       "chapters":2
+       "chapters":[0,53,46,28,34,18,38,51,66,28,29,43,33,34]
     },
     "2Kings":{
        "chapterCount":25,
-       "chapters":2
+       "chapters":[0,18,25,27,44,27,33,20,29,37,36,21,21,25,29,38,20,41,37,37,21,26,20,37,20,30]
     },
     "1Chronicles":{
        "chapterCount":29,
-       "chapters":2
+       "chapters":[0,54,55,24,43,26,81,40,40,44,14,47,40,14,17,29,43,27,17,19,8,30,19,32,31,31,32,34,21,30]
     },
     "2Chronicles":{
        "chapterCount":36,
-       "chapters":2
+       "chapters":[0,17,18,17,22,14,42,22,18,31,19,23,16,22,15,19,14,19,34,11,37,20,12,21,27,28,23,9,27,36,27,21,33,25,33,27,23]
     },
     "Ezra":{
        "chapterCount":10,
-       "chapters":2
+       "chapters":[0,11,70,13,24,17,22,28,36,15,44]
     },
     "Nehemiah":{
        "chapterCount":13,
-       "chapters":2
+       "chapters":[0,11,20,32,23,19,19,73,18,38,39,36,47,31]
     },
     "Esther":{
        "chapterCount":10,
-       "chapters":2
+       "chapters":[0,22,23,15,17,14,14,10,17,32,3]
     },
     "Job":{
        "chapterCount":42,
-       "chapters":2
+       "chapters":[0,22,13,26,21,27,30,21,22,35,22,20,25,28,22,35,22,16,21,29,29,34,30,17,25,6,14,23,28,25,31,40,22,33,37,16,33,24,41,30,24,34,17]
     },
     "Psalms":{
        "chapterCount":150,
-       "chapters":2
+       "chapters":[0,6,12,8,8,12,10,17,9,20,18,7,8,6,7,5,11,15,50,14,9,13,31,6,10,22,12,14,9,11,12,24,11,22,22,28,12,40,22,13,17,13,11,5,26,17,11,9,14,20,23,19,9,6,7,23,13,11,11,17,12,8,12,11,10,13,20,7,35,36,5,24,20,28,23,10,12,20,72,13,19,16,8,18,12,13,17,7,18,52,17,16,15,5,23,11,13,12,9,9,5,8,28,22,35,45,48,43,13,31,7,10,10,9,8,18,19,2,29,176,7,8,9,4,8,5,6,5,6,8,8,3,18,3,3,21,26,9,8,24,13,10,7,12,15,21,10,20,14,9,6]
     },
     "Proverbs":{
        "chapterCount":31,
-       "chapters":2
+       "chapters":[0,33,22,35,27,23,35,27,36,18,32,31,28,25,35,33,33,28,24,29,30,31,29,35,34,29,28,27,28,27,33,31]
     },
     "Ecclesiastes":{
        "chapterCount":12,
-       "chapters":2,
+       "chapters":[0,18,26,22,16,20,12,29,17,18,20,10,14]
     },
     "SongofSolomon":{
        "chapterCount":8,
-       "chapters":2
+       "chapters":[0,17,17,11,16,16,13,13,14]
     },
     "Isaiah":{
        "chapterCount":66,
-       "chapters":2
+       "chapters":[0,31,22,26,6,30,13,25,22,21,34,16,6,22,32,9,14,14,7,25,6,17,25,18,23,12,21,13,29,24,33,9,20,24,17,10,22,38,22,8,31,29,25,28,28,25,13,15,22,26,11,23,15,12,17,13,12,21,14,21,22,11,12,19,12,25,24]
     },
     "Jeremiah":{
        "chapterCount":52,
-       "chapters":2
+       "chapters":[0,19,37,25,31,31,30,34,22,26,25,23,17,27,22,21,21,27,23,15,18,14,30,40,10,38,24,22,17,32,24,40,44,26,22,19,32,21,28,18,16,18,22,13,30,5,28,7,47,39,46,64,34]
     },
     "Lamentations":{
        "chapterCount":5,
@@ -190,15 +190,15 @@ var masterArray =
     },
     "1John":{
        "chapterCount":5,
-       "chapters":2
+       "chapters":[0,10,29,24,21,21]
     },
     "2John":{
        "chapterCount":1,
-       "chapters":2
+       "chapters":[0,13]
     },
     "3John":{
-       "chapterCount":1,
-       "chapters":2
+      "chapterCount":1,
+      "chapters":[0,14]
     },
     "Jude":{
        "chapterCount":1,
@@ -279,7 +279,7 @@ const verseCountForChapter = async (book, chapter, ref) => {
 
     let response = await fetch(`https://api.esv.org/v3/passage/text/?q=${book}+${chapter}`, requestOptions).then(response => response.json()).then(result => {
         let verseCount = result.passages[0].match(/(\[\d{1,4}\])/gm).length;
-        console.log(result.passages[0].match(/(\[\d{1,4}\])/gm), book, chapter, 'verseCount:' + verseCount)
+        console.log(result.passages[0].match(/(\[\d{1,4}\])/gm), result, 'verseCount:' + verseCount)
         ref.push(verseCount)
     })  
 
@@ -309,10 +309,19 @@ const push = (key) => {
     }
 }
 
-//push('1Samuel')
+//push('2Samuel')
+
+var booksCount = 1;
+var secondsFromInit = 0;
+
+const timer = () => {
+   secondsFromInit++
+   setTimeout(timer, 1000)
+}
+timer()
 
 var books = [
-    '1Samuel',        '2Samuel',        '1Kings',       '2Kings',
+   '2Kings',
     '1Chronicles',    '2Chronicles',    'Ezra',          'Nehemiah',
     'Esther',          'Job',             'Psalms',         'Proverbs',
     'Ecclesiastes',    'SongofSolomon', 'Isaiah',        'Jeremiah',
@@ -325,12 +334,33 @@ var books = [
     'Ephesians',       'Philippians',     'Colossians',    '1Thessalonians', 
     '2Thessalonians', '1Timothy',       '2Timothy',     'Titus',
     'Philemon',        'Hebrews',         'James',         '1Peter',
-    '2Peter',         '1John',          '2John',        '3John',
+    '2Peter',         
     'Jude',            'Revelation'
 ];
 
+
+/* RATE LIMITS: 
+*  - 60 requests per minute
+*  - 1,000 requests per hour
+*  - 5,000 per day
+*/
+
 function goNext (prevBook) {
-    var nextBookIndex = (books.indexOf(prevBook) + 1);
-    push(books[nextBookIndex])
+   // Example: booksCount = 1; secondsFromInit = 40;
+   if (booksCount * 60 > secondsFromInit) {
+      let diffSecond = (booksCount * 60 - secondsFromInit) + 1;
+      let diffMs = diffSecond * 1000
+
+      console.log('throttling. Next call in ' + diffSecond + ' seconds OR ' + diffMs + ' milliseconds')
+      setTimeout(() => {
+         goNext(prevBook);
+      }, diffMs);
+
+      return;
+   }
+   booksCount++;
+
+   var nextBookIndex = (books.indexOf(prevBook) + 1);
+   push(books[nextBookIndex])
 }
 
